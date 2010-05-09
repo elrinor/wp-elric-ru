470303871d229a6727e5f867d2ef159bf5557ff6
ru.elric <ru.elric@17c49bba-6213-6a2a-bbcb-87128a5ba983>
2010-05-09 00:20:58 +0000

    + Projects template. + Project information widget. + Selection styling. * Alignclear added where it was needed. * Sorted out margin stuff.

---
+++ scheme/projects.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Projects
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php
+	the_post();
+	global $post_id; $post_id = get_the_ID();
+?>
+
+<?php include(TEMPLATEPATH . '/content-intro.php'); ?>
+	<h1> Major projects </h1>
+	<?php
+		$myposts = get_children('post_parent=' . $post_id);
+		foreach ($myposts as $post) {
+			setup_postdata($post);
+			if (get_post_meta(get_the_ID(), 'is_serious', true) == 0)
+				continue;
+	?>
+		<?php include(TEMPLATEPATH . '/multi-project.php'); ?>
+	<?php } ?>
+	
+	<hr>
+	
+	<h1> Minor projects </h1>
+	<?php
+		$myposts = get_children('post_parent=' . $post_id);
+		foreach ($myposts as $post) {
+			setup_postdata($post);
+			if (get_post_meta(get_the_ID(), 'is_serious', true) == 1)
+				continue;
+	?>
+		<?php include(TEMPLATEPATH . '/multi-project.php'); ?>
+	<?php } ?>
+<?php include(TEMPLATEPATH . '/content-outro.php'); ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
