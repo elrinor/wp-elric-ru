1f4ead07d7e0bd08073d035becc614e66c0546e5
ru.elric <ru.elric@17c49bba-6213-6a2a-bbcb-87128a5ba983>
2010-06-19 15:56:59 +0000

    - Removed contacts tab. + Social footer renamed to contacts, added Gmail and Last.fm links.

--- trunk/scheme/footer.php
+++ trunk/scheme/footer.php
@@ -5,9 +5,11 @@
 			<div id="footer">
 				<div id="footer-inside">
 					<div id="social-box" class="footer-box">
-						<h1>Social</h1>
+						<h1>Contacts</h1>
+						<a id="gmail-link" title="Mail me" href="mailto:ru.elric@gmail.com">GMail</a>
 						<a id="facebook-link" title="Go to my Facebook page" href="http://www.facebook.com/profile.php?id=1630080748">Facebook</a>
 						<a id="picasa-link" title="Go to my Picasa page" href="http://picasaweb.google.com/jp.elric">Picasa</a>
+						<a id="lastfm-link" title="Go to my Last.fm page" href="http://www.last.fm/user/ArX_Elric">Last.fm</a>
 					</div>
 					<div id="powered-box" class="footer-box">
 						<h1>Powered by</h1>
